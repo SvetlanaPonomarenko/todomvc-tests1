6e02ad2e2d83b97afbc23e0d72e69a7e44efc51a
SvetlanaPonomarenko <lana25185@gmail.com>
2020-12-28 03:24:07 +0200

    version with waiting

--- tests/test_todos_management.py
+++ tests/test_todos_management.py
@@ -1,32 +1,30 @@
-import time
 from selene.support.conditions import have
 from selene.support.shared import browser
 
 
 def test_todos():
     browser.open('http://todomvc4tasj.herokuapp.com/')
-    time.sleep(2)
+    browser.should(have.js_returned(True, 'return Object.keys(require.s.contexts._.defined).length === 39;'))
 
+    # Add
     browser.element('#new-todo').type('a').press_enter()
     browser.element('#new-todo').type('b').press_enter()
     browser.element('#new-todo').type('c').press_enter()
-
     browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
 
-    browser.element('#todo-list>li:nth-of-type(2) .view').double_click()
-    browser.element('#todo-list>li:nth-of-type(2) .edit').double_click().type('b new').press_enter()
+    # Edit
+    browser.all('#todo-list>li').element_by(have.exact_text('b')).double_click()
+    browser.all('#todo-list>li').element_by(have.css_class('editing')).element('.edit').type(' new').press_enter()
 
-    browser.all('#todo-list>li').should(have.exact_texts('a', 'b new', 'c'))
-
-    browser.element('#todo-list>li:nth-of-type(2) .toggle').click()
-    browser.all('#todo-list>li.completed').should(have.exact_texts('b new'))
-    browser.all('#todo-list>li:not(.completed)').should(have.exact_texts('a', 'c'))
-
+    # Complete&Clear
+    browser.all('#todo-list>li').element_by(have.exact_text('b new')).element('.toggle').click()
     browser.element('#clear-completed').click()
     browser.all('#todo-list>li').should(have.exact_texts('a', 'c'))
 
-    browser.element('#todo-list>li:nth-of-type(2) .view').double_click()
-    browser.element('#todo-list>li:nth-of-type(2) .edit').double_click().type('c new').press_escape()
-    browser.element('#todo-list>li:nth-of-type(2) .destroy').click()
+    # Cancel edit
+    browser.all('#todo-list>li').element_by(have.exact_text('c')).double_click()
+    browser.all('#todo-list>li').element_by(have.css_class('editing')).element('.edit').type(' new').press_escape()
 
+    # Delete
+    browser.all('#todo-list>li').element_by(have.exact_text('c')).hover().element('.destroy').click()
     browser.all('#todo-list>li').should(have.exact_texts('a'))
